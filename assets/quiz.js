8a7a33c2096f6e692d1d993b2cd574203fbb9690
katguerin <katguerin55@gmail.com>
2020-04-05 18:46:51 -0700

    added first question controls. tried to fix spacing. event listeners.

--- assets/quiz.js
+++ assets/quiz.js
@@ -2,6 +2,8 @@ var buttonEl = document.getElementById("startTestBtn");
 
 var seconds = document.getElementById("timer").textContent;
 var countDown = null;
+var score = 0;
+var answered = false;
 
 function myCountDown() {
     if (countDown==null) {
@@ -16,5 +18,27 @@ function myCountDown() {
     }
 }
 
+function AITest(event) {
+    console.log(event.target.id);
+    if (answered == false){
+        if (event.target.id == "AI2") {
+          score++;
+            document.getElementById("AI-answer").textContent = "CORRECT!";
+        }
+        else {
+           seconds = seconds - 5;
+           document.getElementById("AI-answer").textContent = "WRONG! YOU LOSE 5 seconds!";
+        }
+        answered = true;
+        setTimeout(function(){
+            document.getElementById("questionOne").className = "hidden";
+            document.getElementById("questionTwo").classList.remove("hidden");
+        }, 5000);
+    }
+}
+
 
 buttonEl.addEventListener("click", myCountDown); 
+document.getElementById("AI1").addEventListener("click", AITest);
+document.getElementById("AI2").addEventListener("click", AITest);
+document.getElementById("AI3").addEventListener("click", AITest);
